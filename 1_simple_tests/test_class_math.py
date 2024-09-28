dab569879667ffb34eb1d055df72d55d7cdfeded
Menoudo <priahin@list.ru>
2024-09-28 21:18:31 +0300

    simple_tests

---
+++ 1_simple_tests/test_class_math.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestMathOperations(unittest.TestCase):
+    def setUp(self):
+        self.x = 1
+        self.y = 2
+
+    def tearDown(self):
+        # TODO document why this method is empty
+        pass
+
+    def test_addition(self):
+        result = self.x + self.y
+        self.assertEqual(result, 3)
---
+++ 1_simple_tests/test_func_math.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestMathOperations(unittest.TestCase):
+      def test_addition(self):
+          self.assertEqual(1 + 1, 2)
+
+      def test_subtraction(self):
+          self.assertEqual(5 - 3, 2)
